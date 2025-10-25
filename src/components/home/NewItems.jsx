41643cefc97cd35d223e2ca3df33306f5593a422
Rinwu1114 <Rinwu1114@gmail.com>
2025-10-25 17:01:08 -0500

    bug fiexed hot collections skeleton loading state rendering logic flipped. ereased a console log line.

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const NewItems = () => {
+
+  const [ newItems, setNewItems ] = useState([])
+  const [ isLoading, setIsLoading ] = useState(false)
+
+  useEffect(() => {
+    setIsLoading(true)
+    async function fetchNewItems() {
+      try{
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems"
+        )
+        if( response.data.length > 0 ) {
+          setNewItems(response.data)
+        }
+      } catch (error) {
+        alert("Error Fetching New Items Data", error)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchNewItems()
+  }, []);
+
+  console.log(setNewItems)
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,8 +41,61 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {isLoading ? 
+          new Array(4).fill(0).map((data) => (
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={data.id}>
+              <div className="nft__item">
+                <div className="author_list_pp">
+                  <Skeleton
+                    width={50} height={50} borderRadius="50%" 
+                  />
+                    <i className="fa fa-check"></i>
+                </div>
+                <div className="nft__item_wrap">
+                  <div className="nft__item_extra">
+                    <div className="nft__item_buttons">
+                      <button>Buy Now</button>
+                      <div className="nft__item_share">
+                        <h4>Share</h4>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-facebook fa-lg"></i>
+                        </a>
+                        <a href="" target="_blank" rel="noreferrer">
+                          <i className="fa fa-twitter fa-lg"></i>
+                        </a>
+                        <a href="">
+                          <i className="fa fa-envelope fa-lg"></i>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+
+                  <Skeleton>
+                    <img
+                      src={nftImage}
+                      className="lazy nft__item_preview"
+                      alt=""
+                    />
+                  </Skeleton>
+                </div>
+                <div className="nft__item_info">
+                  <Skeleton
+                  width={100} height={15} borderRadius={8}
+                  />
+                  <div className="nft__item_price">
+                    <Skeleton
+                    width={75} height={15} borderRadius={8} 
+                    />
+                  </div>
+                  <div className="nft__item_like">
+                    <i className="fa fa-heart"></i>
+                    <span>69</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )) : newItems.map((data) =>(
+            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={data.id}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
